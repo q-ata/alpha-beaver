bca86d0152d6413035d909cdf41249992195e04b
BuildTools <davidhuax@gmail.com>
2021-06-05 15:59:38 -0400

    Some design chances, change to LF

--- frontend/src/components/Class.js
+++ frontend/src/components/Class.js
@@ -1,8 +1,6 @@
 /* eslint-disable */
 import React from 'react';
-import {Link, useHistory} from "react-router-dom";
 import {useEffect, useState} from "react";
-import Cookies from "js-cookie";
 import "../styles/class.css";
 import "../styles/nav-bar.css";
 
@@ -147,16 +145,16 @@ const Class = () => {
 
             <div className="middle-section">
               <div className="outline-box">
-                  <div class="icon-group">
-                    <div class="icon instructors"><img src={tempIcon} align="middle"></img><span>Instructors</span></div>
-                    <div class="icon contact"><img src={tempIcon} align="middle"></img><span>Contact</span></div>
-                    <div class="icon grades"><img src={tempIcon} align="middle"></img><span>Grades</span></div>
-                    <div class="icon content"><img src={tempIcon} align="middle"></img><span>Content</span></div>
-                    <div class="icon forums"><img src={tempIcon} align="middle"></img><span>Forums</span></div>
-                    <div class="icon classroom"><img src={tempIcon} align="middle"></img><span>Classroom</span></div>
+                  <div className="icon-group">
+                    <div className="icon instructors"><div className="icon-container"><img src={tempIcon} align="middle"></img><span>Instructors</span></div></div>
+                    <div className="icon contact"><div className="icon-container"><img src={tempIcon} align="middle"></img><span>Contact</span></div></div>
+                    <div className="icon grades"><div className="icon-container"><img src={tempIcon} align="middle"></img><span>Grades</span></div></div>
+                    <div className="icon content"><div className="icon-container"><img src={tempIcon} align="middle"></img><span>Content</span></div></div>
+                    <div className="icon forums"><div className="icon-container"><img src={tempIcon} align="middle"></img><span>Forums</span></div></div>
+                    <div className="icon classroom"><div className="icon-container"><img src={tempIcon} align="middle"></img><span>Classroom</span></div></div>
                   </div>
                   <div className="outline-title">
-                    <b>Course Outline</b>
+                    Course Outline
                   </div>
                   <div className="outline">
                       {classInfo.outline}
@@ -167,10 +165,10 @@ const Class = () => {
                 <div className="announcements">
                   
                 <div className="announcements-title">
-                  <b>Announcements</b>
-                    <button className="sort-button">
-                      <b>Sort By</b>
-                    </button>
+                  <div className="sort-button">
+                    <span className="sort-header">Sort By</span>
+                  </div>
+                  <div className="announce-header">Announcements</div>
                   </div>
                 </div>
               </div>
@@ -187,99 +185,9 @@ const Class = () => {
                     </div>
                   </div>
                 </div>
-
-                
-
-                
             </div>
     </div>
   );
 }
-
-/*
-    <div className="dash-page">
-      <header className="nav-section">
-        <div className="main-nav">
-          <div className="corner-icon">
-          </div>
-          <ul className="nav-bar">
-            <li className="nav-item">
-              <button className="nav-item-button">
-                <div className="nav-item-icon">
-                  <img src="" alt="" />
-                </div>
-                <div className="nav-item-caption">
-                  Account
-                </div>
-              </button>
-            </li>
-            <li className="nav-item">
-              <button className="nav-item-button">
-                <div className="nav-item-icon">
-                  <img src="" alt="" />
-                </div>
-                <div className="nav-item-caption">
-                  Dashboard
-                </div>
-              </button>
-            </li>
-            <li className="nav-item">
-              <button className="nav-item-button">
-                <div className="nav-item-icon">
-                  <img src="" alt="" />
-                </div>
-                <div className="nav-item-caption">
-                  Courses
-                </div>
-              </button>
-            </li>
-          </ul>
-        </div>
-      </header>
-      <div className="content-wrapper">
-        <div className="area-wrapper">
-          <div className="content-area">
-            <div className="content">
-              <div className="dashboard">
-                <div className="page-header">
-                  <h1 className="header"><span>Dashboard</span></h1>
-                  <div className="header-actions">
-                    <span>
-                      <button></button>
-                    </span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="cards">
-
-                {courses}
-              
-              </div>
-            </div>
-          </div>
-          <div className="rs-bar">
-            <div className="calendar">
-              <img src="" alt="calendar" />
-            </div>
-            <div className="announcements">
-              <div className="announce-header">
-                <span>Announcements</span>
-              </div>
-
-              <ul className="announcements-list">
-                {announces}
-              </ul>
-
-              <div className="announce-show-all">
-                <button>Show All</button>
-              </div>
-              
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    */
 
 export default Class;
