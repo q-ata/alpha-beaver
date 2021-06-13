a6c56922e6a1593da559cda05143f6594ae6911b
BuildTools <davidhuax@gmail.com>
2021-06-13 01:53:11 -0400

    Change back jwt expiry time

--- backend/resolver.js
+++ backend/resolver.js
@@ -24,7 +24,7 @@ const resolve = (api, cPath, endpoints) => {
 };
 
 const generateToken = (user, school) => {
-  return jwt.sign({user, school}, process.env.SECRET_KEY, {expiresIn: 5});
+  return jwt.sign({user, school}, process.env.SECRET_KEY, {expiresIn: 900});
 };
 
 const validateToken = (token) => {
